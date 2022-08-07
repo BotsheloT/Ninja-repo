50ef02143592587e4b3f8548a05fab1c8fd33281
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-07 21:11:12 +0200

    Component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
